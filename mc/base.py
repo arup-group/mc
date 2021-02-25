cc72ded40b9053740442236635b197d2d2d45477
fred.shone <fred.shone@arup.com>
2021-02-25 22:51:25 +0000

    add bitsim step

--- mc/base.py
+++ mc/base.py
@@ -416,6 +416,33 @@ class BaseConfig(Base, BaseDebug):
 
         raise KeyError(f"key:'{key}' not found in modules and not valid")
 
+    def find(self, address: str) -> list:
+
+        list_address = address.strip("/").split("/")
+        
+        if len(list_address) == 1:  # reached end of address
+            address = list_address[0]
+            if address in self.modules:
+                return [self.modules[address]]
+            # if address == "*":
+            #     return list(self.modules.values())
+
+        if list_address[0] in self.modules:
+            found = list_address.pop(0)
+            address = "/".join(list_address)
+            search = [self.modules[found].find(address)]
+            return [f for g in search for f in g]  # flatten
+
+        if list_address[0] == "*":
+            found = list_address.pop(0)
+            address = "/".join(list_address)
+            search = [m.find(address) for m in self.modules.values()]
+            return [f for g in search for f in g]  # flatten
+
+        address = "/".join(list_address)
+        search = [m.find(address) for m in self.modules.values()]
+        return [f for g in search for f in g]  # flatten
+
     def __delitem__(self, key):
         del self.modules[key]
 
@@ -533,6 +560,53 @@ class Module(Base):
 
         raise KeyError(f"key:'{key}' not found in params/sets and not valid")
 
+    def find(self, address: str) -> list:
+
+        list_address = address.strip("/").split("/")
+
+        if len(list_address) == 1:  # reached end of address
+            address = list_address[0]
+            if address in self.params:
+                return [self.params[address]]
+            if address in self.parametersets:
+                return [self.parametersets[address]]
+            if address == "*":
+                search = [ps.find(address) for ps in self.parametersets.values()]
+                return [f for g in search for f in g]  # flatten
+            if "*" in address:
+                snaps = []
+                for candidate in self.parametersets:
+                    if specials_snap(candidate, address):
+                        snaps.append(self.parametersets[candidate])
+                return snaps
+
+        if list_address[0] in self.parametersets:  # regular find recurse
+            found = list_address.pop(0)
+            address = "/".join(list_address)
+            search = [self.parametersets[found].find(address)]
+            return [f for g in search for f in g]  # flatten
+
+        if list_address[0] == "*":
+            found = list_address.pop(0)
+            address = "/".join(list_address)
+            search = [ps.find(address) for ps in self.parametersets.values()]
+            return [f for g in search for f in g]  # flatten
+
+        if "*" in list_address[0]:  # special find recurse
+            snaps= []
+            for candidate in self.parametersets:
+                if specials_snap(candidate, list_address[0]):
+                    snaps.append(self.parametersets[candidate])
+            if snaps:
+                found = list_address.pop(0)
+                address = "/".join(list_address)
+                search = [ps.find(address) for ps in snaps]
+                return [f for g in search for f in g]  # flatten
+
+        address = "/".join(list_address)  # recurse through everything
+        search = [ps.find(address) for ps in self.parametersets.values()]
+        return [f for g in search for f in g]  # flatten
+
     def get(self, key, default=None):
         if key in self.params:
             return self.params[key].value
@@ -619,6 +693,60 @@ class ParamSet(Base):
 
         raise KeyError(f"key:'{key}' not found in params/sets and not valid")
 
+    def find(self, address: str) -> list:
+
+        list_address = address.strip("/").split("/")
+
+        if len(list_address) == 1:  # end recursion as address is all used
+            if address in self.params:
+                return [self.params[address]]
+            if address in self.parametersets:
+                return [self.parametersets[address]]
+            if address == "*":
+                search = [ps.find(address) for ps in self.parametersets.values()]
+                flatten = [f for g in search for f in g]  # flatten
+                return flatten + list(self.params.values())
+            if "*" in address:
+                snaps = []
+                for candidate in self.parametersets:
+                    if specials_snap(candidate, address):
+                        snaps.append(self.parametersets[candidate])
+                return snaps
+
+        if list_address[0] in self.parametersets:
+            found = list_address.pop(0)
+            address = "/".join(list_address)
+            return self.parametersets[found].find(address)
+
+        # not going to look through params as this isn't end of address
+
+        if list_address[0] == "*":
+            found = list_address.pop(0)
+            address = "/".join(list_address)
+            search = [ps.find(address) for ps in self.parametersets.values()]
+            return [f for g in search for f in g]  # flatten
+
+        if "*" in list_address[0]:  # special find recurse
+            snaps= []
+            for candidate in self.parametersets:
+                if specials_snap(candidate, list_address[0]):
+                    snaps.append(self.parametersets[candidate])
+            if snaps:
+                found = list_address.pop(0)
+                address = "/".join(list_address)
+                search = [m.find(address) for m in snaps]
+                return [f for g in search for f in g]  # flatten
+
+        address = "/".join(list_address)  # recurse through everything
+
+        # search = [m.find(address) for m in self.params.values()]
+        # flatten = [f for g in search for f in g]  # flatten
+        # if flatten:
+        #     return flatten
+
+        search = [m.find(address) for m in self.parametersets.values()]
+        return [f for g in search for f in g]  # flatten
+        
     def __setitem__(self, key, value):
 
         if not isinstance(value, (str, ParamSet, Param)):
@@ -683,6 +811,19 @@ class Param(Base):
         return True
 
 
+def specials_snap(a, b, divider=":", ignore="*"):
+    """
+    Special function to check for key matches with consideration of
+    special character '*' that represents 'all'.
+    """
+    list_a = a.split(divider)
+    list_b = b.split(divider)
+    for a, b in zip(list_a, list_b):
+        if not (a == ignore or b == ignore or a == b):
+            return False
+    return True
+
+
 def path_exists(path: Path) -> bool:
     """
     Check if path exists.
@@ -739,9 +880,15 @@ def build_paramset_key(elem: et.Element) -> Tuple[str, str]:
         key = paramset_type + ":" + name
         return paramset_type, key
 
-    if paramset_type in ["scoringParameters", "strategysettings"]:
+    if paramset_type in ["scoringParameters"]:
         name, = [p.attrib['value'] for p in elem.xpath("./param[@name='subpopulation']")]
         key = paramset_type + ":" + name
+        return paramset_type, key
+
+    if paramset_type in ["strategysettings"]:
+        subpop, = [p.attrib['value'] for p in elem.xpath("./param[@name='subpopulation']")]
+        strategy, = [p.attrib['value'] for p in elem.xpath("./param[@name='strategyName']")]
+        key = paramset_type + ":" + subpop + ":" + strategy
         return paramset_type, key
 
     if paramset_type in ["modeMapping"]:
---
+++ mc/bitsim.py
@@ -0,0 +1,118 @@
+import logging
+from pathlib import Path
+
+from mc.base import BaseConfig, Param
+
+
+def step_config(input_file: Path, output_file: Path, overrides: tuple) -> None:
+    """
+    Step a config for bitsim based on overrides map.
+    Note that MATSim configs use paths relative to config location.
+    Args:
+        input_file (Path): Input config path ("mc_input_config")
+        output_file (Path): Output config path ("current matsim")
+        overrides (tuple): Tuple representation of overrides
+    """
+    logging.info(f"Loading config from: {input_file}")
+    config = BaseConfig(input_file)
+
+    logging.info(f"Applying overrides: {overrides}")
+    overrides = construct_override_map_from_tuple(overrides=overrides)
+    set_write_path(config=config, overrides=overrides)
+    set_input_paths(config=config, overrides=overrides)
+    set_last_iteration(config=config, overrides=overrides)
+    find_and_set_overrides(config=config, overrides=overrides)
+
+    logging.info(f"Writing config to: {output_file}")
+    try:
+        output_file.parent.mkdir(parents=True, exist_ok=False)
+    except FileExistsError:
+        logging.error(f"Folder for {output_file} is already there")
+    else: 
+        logging.info(f"Creating dir for {output_file}")
+    config.write(output_file)
+
+
+def construct_override_map_from_tuple(overrides: tuple) -> dict:
+    override_map = {}
+    for i in range(0, len(overrides), 2):
+        override_map[overrides[i]] = overrides[i+1]
+    return override_map
+
+
+def set_write_path(config: BaseConfig, overrides: dict) -> None:
+    logging.info(f"Write path override to config")
+    old_write_path = Path(config['controler']['outputDirectory'])
+    new_write_path = Path(overrides.pop("outputDirectory"))
+    config['controler']['outputDirectory'] = str(new_write_path)
+    logging.info(f"Write file path override: {str(old_write_path)} to: {str(new_write_path)}")
+
+
+def set_input_paths(config: BaseConfig, overrides: dict) -> None:
+    """
+    Change input config path value for <>File names parameters, eg inputNetworkFile.
+    Note that MATSim configs use paths relative to config location.
+    TODO path names can be hardcoded to 'output_plans.xml.gz' and so
+    after 1st biteration.
+    :param config: Config
+    :param overrides: Path of output
+    """
+    logging.info(f"Input path overrides to config")
+    dir = Path(overrides.pop("matsim_source"))
+
+    for _, module in config.modules.items():
+        for param_name, param in module.params.items():
+            """
+            This is a bit dangerous, ie if we add new params in future
+            we need to check they do or don't end in 'File' as appropriate.
+            But on the plus side - this method is also more flexible than
+            hardcoding, which would require us to check which version of 
+            MATSim is being used.
+            """
+            if param_name[-4:] == 'File':
+                if param.value == 'null' or "":
+                    continue
+
+                old_path = Path(param.value)
+                file_name = old_path.name
+                new_path = dir / file_name
+                logging.info(f"Input file path override: {str(old_path)} to: {str(new_path)}")
+                param.value = str(new_path)
+
+
+def set_last_iteration(config: BaseConfig, overrides: dict) -> None:
+    """
+    Set config last iteration to "step".
+    TODO - pretty sure we could set the firstIteration and last Iteration such
+    'biteration' 0 would be firstIteration=0 and lastIteration=10,
+    'biteration' 10 would be firstIteration=10 and lastIteration=20,
+    and so on, which will be nicer for the user. This would require we pass
+    'index' and 'next_index' from the orchestration to the cli.
+    """
+    logging.info(f"Step overrides to config")
+    old_step = config['controler']['lastIteration'] 
+    new_step = overrides.pop('step')
+    config['controler']['lastIteration'] = new_step
+    logging.info(f"LastIteration (step) override: {old_step} to: {new_step}")
+
+
+def find_and_set_overrides(config: BaseConfig, overrides: dict) -> None:
+    """
+    Set mutations (for example from a random or grid search) based on
+    mc addresses in the overrides. For example given:
+    {
+        'planCalcScore/scoringParameters:default/performing': '10',
+        'planCalcScore/scoringParameters:default/activityParams:work/openingTime': '07:00:00',
+        'planCalcScore/scoringParameters:default/modeParams:car/monetaryDistanceRate': '-0,00001',
+        'planCalcScore/scoringParameters:*/modeParams:car/monetaryDistanceRate': '-0,00001',
+        'strategy/fractionOfIterationsToDisableInnovation': '0',
+        'strategy/strategysettings:default/weight': '0'
+    }
+    """
+    for k, v in overrides.items():
+        params = config.find(k)
+        for param in params:
+            if isinstance(param, Param):
+                old_value = param.value
+                param.value = v
+                logging.info(f"Override {param.ident}: {old_value} to: {v}")
