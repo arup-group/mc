68c1d9c93068ca3a02c2f330112d9bc997583ebf
Jade Mackay <jademackay@gmail.com>
2021-10-13 06:46:14 +1300

    Add failing test entries for `fill.paramreplace` and update naming for `fill.matchreplace`

--- tests/test_filling.py
+++ tests/test_filling.py
@@ -1,37 +1,51 @@
 import filecmp
 import os
 
-from mc.fill import fill_config
+from mc.fill import match_replace
 
-def test_update_config_update_one_matching_fill():    
+
+def test_matchreplace_config_update_one_matching_fill():
     in_file = "tests/test_data/test_wildcard.json"
     out_file = "tests/test_data/test_wildcard_out.json"
     correct_ouput = "tests/test_data/test_wildcard_filled.json"
     overrides = ["wildcard", "filled"]
 
-    fill_config(in_file, out_file, overrides)
+    match_replace(in_file, out_file, overrides)
 
     assert filecmp.cmp(out_file, correct_ouput)
     os.remove(out_file)
 
-def test_update_config_update_one_matching_fill_xml():
+
+def test_matchreplace_config_update_one_matching_fill_xml():
     in_file = "tests/test_data/test_wildcard.xml"
     out_file = "tests/test_data/test_wildcard_out.xml"
     correct_ouput = "tests/test_data/test_wildcard_filled.xml"
     overrides = ["wildcard", "filled"]
 
-    fill_config(in_file, out_file, overrides)
+    match_replace(in_file, out_file, overrides)
 
     assert filecmp.cmp(out_file, correct_ouput)
     os.remove(out_file)
 
 
-def test_update_config_no_update_when_no_match():
+def test_matchreplace_config_no_update_when_no_match():
     in_file = "tests/test_data/test_wildcard.json"
     out_file = "tests/test_data/test_wildcard_out.json"
     overrides = ["matchless", "filled"]
 
-    fill_config(in_file, out_file, overrides)
+    match_replace(in_file, out_file, overrides)
 
     assert filecmp.cmp(out_file, in_file)
     os.remove(out_file)
+
+
+def test_paramreplace_config_update_one_matching_fill():
+    assert 1 == 2
+
+
+def test_paramreplace_config_update_one_matching_fill_xml():
+    assert 1 == 2
+
+
+def test_paramreplace_config_no_update_when_no_match():
+    assert 1 == 2
