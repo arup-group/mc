0bb99e2cd2cd22aa21608acdd48dfd387d44dcf2
Yuhao Sun <syhwawa@gmail.com>
2022-01-05 10:44:12 +0000

    add space in comments

--- mc/summarise.py
+++ mc/summarise.py
@@ -7,24 +7,24 @@ def write_summary_log(config, output_path):
     """
     message = []
     
-    #add paths of the input files
+    # add paths of the input files
     message.append("{:=^100s}".format("input files"))
     message.append(f"network_path:{config['network']['inputNetworkFile']}")
     message.append(f"plans_path:{config['plans']['inputPlansFile']}")
     message.append(f"schedule_path:{config['transit']['transitScheduleFile']}")
     message.append(f"vehicles_path:{config['transit']['vehiclesFile']}")
     
-    #add mobsim setting summary
+    # add mobsim setting summary
     message.append("{:=^100s}".format("mobsim setting"))
     message.append(f"mobsim:{config['controler']['mobsim']}")
     message.append(f"Flow_Capacity_Factor:{config[config['controler']['mobsim']]['flowCapacityFactor']}")
     message.append(f"Storage_Capacity_Factor:{config[config['controler']['mobsim']]['storageCapacityFactor']}")
     
-    #check mode choice
+    # check mode choice
     message.append("{:=^100s}".format("mode"))
     message.append(f"{config['subtourModeChoice']['modes']}")
 
-    #summarise the scoring parameters for each mode 
+    # summarise the scoring parameters for each mode 
     message.append("{:=^100s}".format("parameters for scoring"))
     for i in config['subtourModeChoice']['modes'].split(','):
         message.append("{:-^100s}".format("mode:"+ str(i)))
@@ -33,7 +33,7 @@ def write_summary_log(config, output_path):
         message.append(f"marginal_utility_of_traveling:{config['planCalcScore']['scoringParameters:default']['modeParams:'+ str(i)]['marginalUtilityOfTraveling_util_hr']}") 
         message.append(f"monetary_distance_rate:{config['planCalcScore']['scoringParameters:default']['modeParams:'+ str(i)]['monetaryDistanceRate']}") 
     
-    #write the above key information to a text file
+    # write the above key information to a text file
     textfile = open(os.path.join(output_path,'simulation_log.txt'), 'w')
     for element in message:
         textfile.write(element + "\n")
