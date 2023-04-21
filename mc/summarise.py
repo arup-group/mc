0bd5c6cfabf62dcda2688b5f6d7343722704eac9
syhwawa <syhwawa@gmail.com>
2023-04-21 14:19:28 +0100

    add the updated simulation log example

--- mc/summarise.py
+++ mc/summarise.py
@@ -37,7 +37,8 @@ def directory_log_summary(config):
 
 def scoring_summary(config):
     """
-    Display a table with scoring parameters for different modes and subpopulations in tabular format    """
+    Display a table with scoring parameters for different modes and subpopulations in tabular format    
+    """
     message = directory_log_summary(config)
     # add a header for the mode section
     message.append("{:=^150s}".format(" mode "))
